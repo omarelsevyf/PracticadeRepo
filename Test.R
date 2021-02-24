b78f970d0e92fc36260b8de17837a975e679e6d5
Omar Elsevyf <omar.elsevyf@hotmail.com>
2021-02-23 23:00:29 -0400

    Commit del branch

--- Test.R
+++ Test.R
@@ -1,8 +1,6 @@
 x = 1:5;
 y = 1:5;
 plot(x,y)
-#Septima prueba
-#Edicion en Github visualizate al farol
 z = x + y
-#edicion once again in GitHub
-#con eta edicion se supone que se ve el commit desde Github
+#eto va pal branch
+
